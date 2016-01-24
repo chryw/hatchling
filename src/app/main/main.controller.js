04d9ee97f8fd3b1ea0581ac8ba56f72f209bae3f
Cherry Wang <cherryw0802@gmail.com>
2016-01-23 22:42:25 -0800

    updated logo for favicon and better render result at small size; linked json data to MainController;tested some basic style customization

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,13 +6,14 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($http, $timeout, webDevTec, toastr) {
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1453598456532;
     vm.showToastr = showToastr;
+    vm.test = "Bunny!";
 
     activate();
 
@@ -21,10 +22,11 @@
       $timeout(function() {
         vm.classAnimation = 'rubberBand';
       }, 4000);
+      getJson();
     }
 
     function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+      toastr.info('Fork <a href="https://github.com/chryw/hatchling" target="_blank"><b>hatchling</b></a>');
       vm.classAnimation = '';
     }
 
@@ -33,6 +35,15 @@
 
       angular.forEach(vm.awesomeThings, function(awesomeThing) {
         awesomeThing.rank = Math.random();
+      });
+    }
+
+    function getJson(){
+      vm.json = {};
+      vm.repeatlist = [];
+      $http.get('../../data/content-main.json').success(function(data){
+        vm.json = data[0];
+        vm.repeatlist = data[0].products;
       });
     }
   }
